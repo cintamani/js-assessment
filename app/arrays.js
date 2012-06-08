0254546a0baa93d92738c1c0dadf7c75261034bd
Luca Belmondo <l.belmondo@nature.com>
2012-06-08 21:46:50 +0100

    some solutions

--- app/arrays.js
+++ app/arrays.js
@@ -3,47 +3,92 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+      return arr.indexOf(item);
     },
 
     sum : function(arr) {
-
+      var i = 0, total = 0;
+      for (i; i < arr.length; i += 1) {
+        total += arr[i];
+      }
+      return total;
     },
 
     remove : function(arr, item) {
-
+      var index = arr.indexOf(item);
+      if (index !== -1) { arr.splice(index, index) }
+      return arr;
     },
 
     append : function(arr, item) {
-
+      arr.push(item);
+      return arr;
     },
 
     truncate : function(arr) {
-
+      arr.pop();
+      return arr;
     },
 
     concat : function(arr1, arr2) {
-
+      return arr1.concat(arr2);
     },
 
     insert : function(arr, item, index) {
-
+      arr.splice(index, 0, item);
+      return arr;
     },
 
     count : function(arr, item) {
-
+      var i = 0, count = 0;
+      for (i; i < arr.length; i += 1) {
+        if (arr[i] === item) { count += 1; }
+      }
+      return count;
     },
 
     duplicates : function(arr) {
+      var
+        i             = 0,
+        result        = [],
+        not_in_result = false,
+        is_in_array   = false,
+        not_current   = false,
+        other_index   = 0,
+        item;
 
+      for (i; i < arr.length; i += 1) {
+        item          = arr[i];
+        other_index   = arr.indexOf(item);
+        not_current   = other_index !== i;
+        not_in_result = result.indexOf(item) === -1;
+        is_in_array   = other_index >= 0;
+
+        if (not_current && not_in_result && is_in_array) { result.push(arr[i]); }
+      };
+
+      return result;
     },
 
     square : function(arr) {
+      var i = 0, result = [], number;
 
+      for (i; i < arr.length; i += 1) {
+        number = arr[i];
+        result.push(number * number);
+      }
+
+      return result;
     },
 
     findAllOccurrences : function(arr, target) {
+      var i = 0, result = [];
 
+      for (i; i < arr.length; i += 1) {
+        if (arr[i] === target) { result.push(i); }
+      }
+
+      return result;
     }
   };
 });
--- app/functions.js
+++ app/functions.js
@@ -1,37 +1,70 @@
+/*jslint white:true*/
+/*global define, require, module*/
+
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
-  return {
-    argsAsArray : function(arr) {
+  "use strict";
 
+  return {
+    argsAsArray : function(fn, arr) {
+      return fn.apply(this, arr);
     },
 
     speak : function(fn, obj) {
-
+      return fn.call(obj);
     },
 
-    functionFunction : function() {
-
+    functionFunction : function(greeting) {
+      return function (name) {
+        return greeting + ", " + name;
+      };
     },
 
-    partial : function() {
-
+    partial : function(fn, greeting, name) {
+      return function (punctuation) {
+        return fn.call(this, greeting, name, punctuation);
+      };
     },
 
     useArguments : function() {
-
+      var i = 0, result = 0, args = Array.prototype.slice.apply(arguments);
+      for (i; i < args.length; i += 1) { result += args[i]; }
+      return result;
     },
 
-    callIt : function() {
-
+    callIt : function(fn) {
+      // take starting from the second because the first is 'fn'
+      var args = Array.prototype.slice.call(arguments, 1);
+      return fn.apply(this, args);
     },
 
-    curryIt : function() {
-
+    curryIt : function(fn) {
+      // take starting from the second because the first is 'fn'
+      var args = Array.prototype.slice.call(arguments, 1);
+      return function () {
+        args.push.apply(args, Array.prototype.slice.call(arguments));
+        return fn.apply(this, args);
+      };
     },
 
-    makeClosures : function() {
+    makeClosures : function(arr, fn) {
+      var i = 0, functions = [], function_factory;
 
+      // create function with a factory, to avoid defining the functions
+      // in the loop
+      function_factory = function (arg) {
+        return function () {
+          var x = arg;
+          return fn(x);
+        };
+      };
+
+      for (i; i < arr.length; i += 1) {
+        functions.push(function_factory(arr[i]));
+      }
+
+      return functions;
     }
   };
 });
