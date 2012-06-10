6396294be483d3bb8be09078e93a9dd620796096
Luca Belmondo <l.belmondo@nature.com>
2012-06-10 16:39:27 +0100

    completed

--- app/async.js
+++ app/async.js
@@ -1,11 +1,28 @@
-define(function() {
+
+define(["jquery"], function($) {
   return {
     async : function() {
+      var promise = new $.Deferred();
 
+      promise.resolve(true);
+
+      return promise;
     },
 
-    manipulateRemoteData : function() {
+    manipulateRemoteData : function(url) {
+      var promise = new $.Deferred();
 
+      $.getJSON(url, function (data) {
+        var i = 0, result = [], people = data.people;
+
+        for (i; i < people.length; i += 1) {
+          result.push(people[i].name);
+        }
+
+        promise.resolve(result.sort());
+      });
+
+      return promise;
     }
   };
 });
--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.apply(obj, arguments);
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
+      var result = [], property;
 
+      for (property in obj) {
+        if (obj.hasOwnProperty(property)) {
+          result.push(property + ": " + obj[property]);
+        }
+      }
+
+      return result;
     }
   };
 });
